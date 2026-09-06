b2c521a68702b571120575c106063534ea7e6da3
inftecnologydegree <yotbe.stream@gmail.com>
2026-09-06 18:34:37 -0300

    feat: url ponteiro

--- public/app.js
+++ public/app.js
@@ -2,7 +2,7 @@
 // Remova o localhost:3000 e coloque o seu link oficial do Render
 const API_URL = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1" || window.location.protocol === "file:"
     ? "http://localhost:3000" 
-    : "https://back-end-node-api.onrender.com"; // 👈 Atualizado com o link gerado pelo Render
+    : "https://data-mining-game-main.onrender.com"; // 👈 Atualizado com o link gerado pelo Render
 
 
 // Seletores do DOM
